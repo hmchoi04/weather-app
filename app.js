3bc5e3b2d3cb4cf0067e9a75ab96434520004fca
Hyun Min Choi <hmchoi@berkeley.edu>
2021-03-15 23:33:01 -0700

    initial settings and layout for project set up

---
+++ app.js
@@ -0,0 +1,12 @@
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
+
+const weather = {};
+weather.temperature = {
+    unit: 'celsisus'
+}
+
+const KLEVIN = 273; 
+
