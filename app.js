921dca4e4b20893971a7f30912730083f567c646
Hyun Min Choi <hmchoi@berkeley.edu>
2021-03-16 21:35:52 -0700

    seting up functions for API calls

--- app.js
+++ app.js
@@ -9,4 +9,36 @@ weather.temperature = {
 }
 
 const KLEVIN = 273; 
+// const key = ; need to get API key 
 
+
+if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}
+else {
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = "<p>Browser doesn't support geolocation</p>";
+}
+
+function setPosition(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    getWeather(latitude, longitude);
+}
+
+function showError(error) {
+    notificationElement.style.dispay = "block";
+    notificationElement.innerHTML = "<p>${error.message}</p>";
+}
+
+function getWeather(latitude, longitude) {
+    // let api = get api key
+    fetch(api).then(function(response){
+        let data = response.json();
+        return data;
+    })
+    .then(function(data){
+        weather.temperature.value = Math.floor(data.min.temp - KELVIN);
+        
+    })
+}
